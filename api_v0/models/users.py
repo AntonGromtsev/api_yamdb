7cc7b0c0d1e32f58c013b9c732c4d9511559120f
AntonGromtsev <fenix217grom@gmail.com>
2021-02-17 17:06:58 +0300

    user

--- api_v0/models/users.py
+++ api_v0/models/users.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+class MyUserManager(BaseUserManager):
+
+    def _create_user(self, email, **kwargs):
+        if not email:
+            raise ValueError(_('The Email must be set')))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, **kwargs):
+        return self._create_user(self, email, is_superuser=False)
+
+    def create_superuser(self, email, **kwargs):
+        return self._create_user(self, email, ,is_staff=True, is_superuser=True)
+
+
+
 
 # Create your models here.
