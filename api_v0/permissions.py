409c2a0a4368e65a736ba8f6837359ad014cdcb3
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-02-23 14:33:22 +0300

    Добавил ручку users/me

--- api_v0/permissions.py
+++ api_v0/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 
 class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
@@ -13,10 +14,16 @@ class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
-        if request.user.is_authenticated:
-            return bool(request.user.is_staff or request.user.role == 'admin')
+        if not request.user.is_authenticated:
+            return False
+        return request.user.is_staff or request.user.role in ('admin')
+
+
+class IsAuthenticatedOrAdmin(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return super(IsAuthenticated, self).has_permission(request, view)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -24,4 +31,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.user.is_authenticated:
-            return bool(request.user.is_staff or request.user.role == 'admin')
+            return request.user.is_staff or request.user.role == 'admin'
--- api_v0/views/users.py
+++ api_v0/views/users.py
@@ -1,12 +1,12 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.hashers import make_password
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from django.shortcuts import get_object_or_404
-from ..permissions import IsAdmin
+from ..permissions import IsAdmin, IsAuthenticatedOrAdmin
 
 from ..models.users import MyUser, UserRegistration
 from ..serializers.users import (
@@ -58,9 +58,26 @@ def get_token(request):
 
 
 class MyUserViewSet (ModelViewSet):
-    permission_classes = [IsAdmin]
     queryset = MyUser.objects.all()
     serializer_class = MyUserSerializer
     lookup_field = 'username'
     filter_backends = [DjangoFilterBackend]
     search_fields = ['user__username', ]
+
+    @action(detail=False, methods=['GET', 'PATCH', 'DELETE'])
+    def me(self, request):
+        if request.method == 'DELETE':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        user = request.user
+        serializer = MyUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def get_permissions(self):
+        if self.action == 'me':
+            permission_classes = [IsAuthenticatedOrAdmin]
+        else:
+            permission_classes = [IsAdmin]
+        return [permission() for permission in permission_classes]
