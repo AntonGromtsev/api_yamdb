cca9087d47e40e031dc907ce65e3465568cbe582
AntonGromtsev <fenix217grom@gmail.com>
2021-02-23 22:45:30 +0300

    настройки settings для отправки сообщений

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from dotenv import load_dotenv
 
-#from api_v0.models.users import MyUser
+# from api_v0.models.users import MyUser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
