d6180c6ea3703d1b5578eddcaa7dac4f4908768f
Ирина Бобина <bobin.ir495@gmail.com>
2021-02-27 22:07:19 +0300

    j4

--- api_v0/views/genres.py
+++ api_v0/views/genres.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters, status
+from rest_framework import viewsets, filters, status, mixins
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -6,8 +6,11 @@ from ..models.genres import Genre
 from ..serializers.genres import GenreSerializer
 from ..permissions import IsAdminOrReadOnly
 
+class CreateGenreViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                             mixins.ListModelMixin, viewsets.GenericViewSet):
+    pass
 
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(CreateGenreViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = PageNumberPagination
@@ -16,8 +19,3 @@ class GenreViewSet(viewsets.ModelViewSet):
     search_fields = ['name', ]
     lookup_field = 'slug'
 
-    def retrieve(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
